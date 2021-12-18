a45838a893cebd014a3413bcaa780e9ea0c195e9
Aminaehb <71768425+Aminaehb@users.noreply.github.com>
2021-12-18 20:55:25 +0100

    Return all images from db with get req

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ app.use(bodyParser.json()); //every body is converted to json
 app.use(express.static('public')) //express nees to use these things
 // instead of localhost:3000/data =  localhost:3000/info.html
 
-
-
 //ROUTE waits for local root= localhost3000 
 
 //GET ROUTE --> call api to do get req
@@ -30,22 +28,37 @@ app.get('/', (req, res) => { //waiting for a get request when we enter a url
     //res.send('Hello Amina!') // req send to server with res
     res.status(300).redirect('/info.html');
 })
+//Return all images from db
+app.get('/images', async (req, res) => {
+    //Read the file
+    try { //async function (await is used)
+        await client.connect(); //connect to the database
+        const db = client.db("course_project").collection("images"); //var connect
+        const data = await collection.find({}).toArray(); //await to find the file
 
-//GET ROUTE --> send data
-app.get('/data', (req, res) => { //waiting for a get request when we enter a url
-    let Data = { //object with details
-        name: 'Amina',
-        age: 19
+         //if it succeeds --> send back the file
+        console.log(data);
+        res.status(200).send(data);
+
+    } catch (error) { //catch an error
+        res.status(500).send('File could not be read! Try again later...')
+        console.log(error.stack);
+    } finally {
+        await client.close();
     }
-    res.send(Data); //send back data --> localhost:3000/data
-})
+
+});
+
+
+
+
 
 //CREATE NEW ROUTE
 // POST method route
 app.post('/saveData', function (req, res) {
     console.log(req.body) //body paramater of req --> by adding it in postman
 
-   // res.send(`Data received`) //json code from postman --> sended to vsc 
+    // res.send(`Data received`) //json code from postman --> sended to vsc 
     res.send(`Data received with id: ${req.body.id}`) //code seen on postman
 
 })
@@ -59,3 +72,14 @@ app.listen(port, () => { //start server on port & do something when its done
 
 //run the server = nodemon index.js
 // POSTMan to test the ROUTS
+
+
+
+//GET ROUTE --> send data
+//app.get('/data', (req, res) => { //waiting for a get request when we enter a url
+//let Data = { //object with details
+// name: 'Amina',
+//  age: 19
+// }
+// res.send(Data); //send back data --> localhost:3000/data
+//})
